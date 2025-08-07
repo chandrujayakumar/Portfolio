a4f003e9380a193aae4d26bc0877e1bbb8b9e69d
chandrujayakumar <chandrujayakumar987@gmail.com>
2025-08-07 14:28:45 +0530

    feat(header): add responsive navigation menu for desktop and mobile with smooth dropdown animation

--- Portfolio/src/compounents/Header.jsx
+++ Portfolio/src/compounents/Header.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import { Menu } from "lucide-react";
+import React, { useEffect, useState } from 'react'
+import { Menu,X } from "lucide-react";
 
 
 function Header() {
 
-      
+      const [isMenuOpen, setIsOpen] = useState(false);
+      const [isScrolled, setIsScrolled] = useState(false);
 
+      useEffect(() => {
+        const handleScroll = () => {
+          setIsScrolled(window.screenY > 50);
+        }
+        window.addEventListener("scroll",handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+      }, []);
+
+      const scrollToSection = (href) => {
+        const element = document.querySelector(href);
+        if(element){
+          element.scrollIntoView({ behavior: "smooth"});
+        }
+        setIsOpen(false);
+      } ;
+
+      const navItems = [
+        { name: "Home", href: "#home" },
+        { name: "About", href: "#about" },
+        { name: "Skills", href: "#skills" },
+        { name: "Projects", href: "#projects" },
+        { name: "Experience", href: "#experience" },
+        { name: "Contect", href: "#contact" },
+
+      ]
   return (
-    <header className={'fixed top-0 left-0 right-0 z-50 transition-all duration-500'}>
+    <header className= {`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
+      isScrolled
+        ?"bg-slate-950 backdrop-blur-lg shadow-lg border-b border-slate-800"
+        : "bg-transparent"
+    }`}
+    >
+     
       <div className='container mx-auto px-5 py-4'>
       <div className='flex items-center justify-between'>
       <div className='text-2xl font-bold'>
@@ -19,8 +51,18 @@ function Header() {
 
       {/*Desktop Menus */}
       <nav className='hidden md:flex space-x-8'>
-      {/* later */}
+      {navItems.map((item, intex)=>{
+        return (
+         <button className='text-gray-300 hover:text-green-400 transition-all duration-300
+            font-medium relative group' style={{animationDelay: `${intex * 0.1}s`}}>
+          {item.name}
+          <span className='absolute -bottom-2 left-0 w-0 h-0.5 bg-green-400
+          transition-all duration-300 group-hover:w-full'></span>
+          </button>
+        );
+      })}
       </nav>
+
       <button className='hidden md:flex items-center gap-2 bg-green-600 text-white px-6 
       py-2.5 rounded-lg hover:bg-green-700 transition-all duration-300 font-medium
       shadow-lg hover:shadow-xl hover:scale-105'>
@@ -28,15 +70,30 @@ function Header() {
       </button>
 
       {/* for Mobile Menu */}
-      <button className='text-white md:hidden'>
-        <Menu size={24}/>
+      <button 
+        className='text-white md:hidden' 
+        onClick={() => setIsOpen(!isMenuOpen)}
+      >
+        {isMenuOpen ? <X size={24} /> : <Menu size={24}/>}
       </button>
       </div>
       {/* Mobile Navigation */}
-      <nav className='md:hidden mt-4 pb-4 border-t border-slate-800 pt-4 bg-slate-800
-      rounded-lg shadow-lg'>
-        {/* Later */}
-       {/*Container aligned to the top-right corner --> */} 
+      <nav
+  className={`
+    md:hidden overflow-hidden transition-all duration-500 ease-in-out 
+    bg-slate-800 rounded-lg shadow-lg mt-4 border-t border-slate-800 pt-4
+    ${isMenuOpen ? "max-h-[500px] opacity-100 scale-100" : "max-h-0 opacity-0 scale-95"}
+  `}
+>
+        {navItems.map((item) => {
+          return (
+            <button className='block w-fll text-left py-3 px-4 text-gray-300
+            hover:text-green-400 hover:bg-slate-700 transition-all duration-300
+            ease-in-out rounded-lg'>
+              {item.name}
+            </button>
+          )
+        })}
 
 
         <button className='mt-4 w-full flex items-center justify-center gap-2 bg-green-600
