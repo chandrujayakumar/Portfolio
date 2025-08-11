d1de3ee22ed351b479cdf07e8ec623cab2ee9b31
chandrujayakumar <chandrujayakumar987@gmail.com>
2025-08-11 11:02:55 +0530

    Animation for background elements

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './compounents/Header'
+import Hero from './compounents/Hero'
 
 function App() {
   return (
     <div className='bg-gray-300'>
       <Header/>
+      <Hero />
     </div>
   )
 }
---
+++ Portfolio/src/compounents/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section
+    id="home"
+    className='min-h-screen flex items-center bg-gradient-to-br from-slate-900
+    to-slate-900 pt-20 relative overflow-hidden'
+    >
+        {/*Animation for background elements*/}
+        <div className='absolute inset-0'>
+            <div className='absolute top-20 left-10 w-72 h-72 bg-green-600/10 rounded-full
+            blur-3xl'></div>
+            <div className='absolite bottom-20 right-10 w-96 h-96 bg-blue-600/10 rounded-full
+            blur-3xl'></div>
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+            2 w-full h-full bg-gradient-to-r from-transparent via-green-600/s to-transparent'></div>
+        </div>
+
+        {/* <div className='container mx-auto'>
+
+        </div> */}
+    </section>
+  )
+}
+ 
+export default Hero
